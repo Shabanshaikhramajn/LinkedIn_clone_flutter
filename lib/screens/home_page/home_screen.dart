4a23dc6afabbeb45f2369459549553ddbaedb98f
shabanshaikhramjn <shabanshaikh72345@gmail.com>
2025-01-01 19:25:01 +0530

    lst commit

--- lib/screens/home_page/home_screen.dart
+++ lib/screens/home_page/home_screen.dart
@@ -83,6 +83,7 @@ _scrollController.dispose();
 
             color: Colors.black12,
             child: Column(
+
               children: [
                 _showAppNavBar
                 ? CustomAppbar(sizingInformation: sizingInformation)
@@ -113,7 +114,115 @@ _scrollController.dispose();
 
       itemBuilder: (context, index) {
 
-          return Container();
+          return Container(
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+            margin: EdgeInsets.only(bottom: 0, top: 8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border(
+                top: BorderSide(color: Colors.black54, width: .50),
+                bottom: BorderSide(
+                  color: Colors.black54,
+                  width: .50
+                )
+              )
+            ),
+            child: Column(
+              children: [
+                    Row(children: [
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage(
+                              _post[index].profileUrl!,
+                            ))
+                        ),
+                      ),
+                      SizedBox(width: 8,)
+                      ,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(_post[index].name!,
+                          style: TextStyle(
+
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+
+                          ),
+                          ),
+                          Container(
+                            width: sizingInformation.screenSize.width/1.34,
+                            child: Text(
+                              _post[index].headline!,
+                              style: TextStyle(
+
+                                overflow: TextOverflow.ellipsis,
+                                fontSize: 12, 
+                                color: Colors.black54
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],),
+                    SizedBox(height: 10,),
+                    Text(_post[index].description!,
+                    style: TextStyle(
+                      fontSize: 14,
+
+                    ),),
+                    
+                    
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(_post[index].description!,
+                      style: TextStyle(
+                        fontSize: 14,
+                      
+                      ),),
+                    ),
+                    SizedBox(height: 10,),
+                    Container(
+                      width: sizingInformation.screenSize.width,
+                      child: Image.asset(_post[index].image!,
+                        fit: BoxFit.contain,
+
+                      ),
+                    ),
+
+                    SizedBox(height: 10,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 25,
+                          height: 25,
+                          child: Image.asset("assets/icons/like_icon.png"),
+                        ),
+                         Container(
+                          width: 25,
+                          height: 25,
+                          child: Image.asset("assets/icons/celebrate_icon.png"),
+                        ),
+                        if(index == 0 || index == 4)
+                         Container(
+                          width: 25,
+                          height: 25,
+                          child: Image.asset("assets/icons/celebrate_icon.png"),
+                        ),
+                      ],
+                    )
+
+                    
+
+              ],
+            ),
+          );
 
       }),
      ));
